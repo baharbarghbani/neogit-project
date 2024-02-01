536b0a6a5cd182524f6df603e51818de41683e2a
Bahar Barghbani <bahar.brqbni@gmail.com>
2024-02-01 20:11:07 +0330

    add is started

--- add.c
+++ add.c
@@ -1,8 +1,33 @@
 #include "add.h"
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include <dirent.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include<time.h>
+void repo_check(bool exist, 
 int add(int argc, char const* argv[])
 {
+    char* address = (char*)malloc(2000);
+    char* repo_address = (char*)malloc(2000);
+    char* cwd = (char*)malloc(2000);
+    char* tmp_cwd = (char*)malloc(2000);
+    bool found = false;
+    bool exists = false;
+    bool is_file = false;
+    bool is_directory = false;
+    bool is_valid = false;
+    struct dirent* entry;
+    
+    if(getcwd(cwd, 2000) == NULL)
+    {
+        fprintf(stderr, "Error in current directory\n");
+        return 1;
+    }
 
-              
 
 
 
---
+++ repo_check.c
@@ -0,0 +1,17 @@
+#ifndef _check 
+#define _check
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include <dirent.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include<time.h>
+void repo_check(bool exists, char* address, char* repo_address)
+{
+    
+}
+#endif
